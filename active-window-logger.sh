d2c353d21bf514614f683c40635277a481ac7e15
Robert Bolender <robert@bolender.email>
2024-01-11 21:17:01 -0800

    Move log to script directory

--- active-window-logger.sh
+++ active-window-logger.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Get the parent directory of the current script
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 # Get the title of the frontmost application window
-window_title=$(osascript /Users/robertbolender/code/activity-tracking/get-window-title.applescript)
+window_title=$(osascript "${script_dir}"/get-window-title.applescript 2>&1)
 
 # Log the title with timestamp to a file
-echo "$(date '+%Y-%m-%d %H:%M:%S') - $window_title" >>~/log/active-window.log
+echo "$(date '+%Y-%m-%d %H:%M:%S') - $window_title" >>"${script_dir}"/active-window.log
