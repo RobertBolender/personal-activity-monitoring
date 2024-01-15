75029b74631bd828bf714c1f32ff6b3811c85582
Robert Bolender <robert@bolender.email>
2024-01-14 22:24:45 -0800

    Log to a new file each day

--- active-window-logger.sh
+++ active-window-logger.sh
@@ -6,5 +6,11 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # Get the title of the frontmost application window
 window_title=$(osascript "${script_dir}"/get-window-title.applescript 2>&1)
 
+monthstamp=$(date '+%Y-%m')
+datestamp=$(date '+%Y-%m-%d')
+timestamp=$(date '+%H:%M:%S')
+
 # Log the title with timestamp to a file
-echo "$(date '+%Y-%m-%d %H:%M:%S') - $window_title" >>"${script_dir}"/active-window.log
+log_dir="${script_dir}"/log/"${monthstamp}"
+mkdir -p "${log_dir}"
+printf "%s %s\t%s\n" "$datestamp" "$timestamp" "$window_title" >>"${log_dir}"/"${datestamp}".log
